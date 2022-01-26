3986bbe3869d54fd01c92f967503ae6b88a983a8
Alexander Fadeev <zander.fadeev@gmail.com>
2022-01-26 22:58:33 +0300

    Add VS Code run infra; Add solution file template

---
+++ template.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+#define LL long long
+#define FOR(T, i, l, r) for (T i = l; i < r; i++)
+#define SORT(x) sort(x.begin(), x.end())
+#define RSORT(x) sort(x.rbegin(), x.rend())
+#define READN(x, n) FOR(size_t, i, 0, n) {cin >> x[i];}
+
+void run() {
+    int n;
+    cin >> n;
+}
+
+int main() {
+    // int t = 1;
+    // int t; cin >> t;
+    while (t--) {
+        run();
+    }
+}
